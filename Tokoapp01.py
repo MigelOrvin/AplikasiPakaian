c5eeba885bccc1f9ed74394a4e912121dbac48cc
MigelOrvin <migel8206@gmail.com>
2020-11-22 10:34:44 +0800

    Fixing bug and Screenshot

--- Tokoapp01.py
+++ Tokoapp01.py
@@ -21,10 +21,11 @@ def verify_ans(char):
 	else:
 		return False
 
-def print_data(daftar_pakaian=None, harga=True, all_data=False):
-	if daftar_pakaian != None and all_data == False:
-		print(f"Jenis Pakaian : {daftar_pakaian}")
-		print(f"HARGA : {daftar_pakaians[daftar_pakaian]['Harga']}")
+def print_data(id_contact=None, harga=True, all_data=False):
+	if id_contact != None and all_data == False:
+		print(f"ID : {id_contact}")
+		print(f"Jenis Pakaian : {daftar_pakaians}")
+		print(f"HARGA : {daftar_pakaians['Harga']}")
 	elif all_data == True:
 		for every_pakaian in daftar_pakaians:
 			Jenis_pakaian = every_pakaian
@@ -40,12 +41,12 @@ def view_daftar_pakaian():
 		print("Maaf tidak ada pakaian yang tersimpan")
 	input("Tekan ENTER untuk kembali ke MENU")
 
-def create_id_contact(name, harga):
+def create_id(name, harga):
 	hari_ini = datetime.now()
 	tahun = hari_ini.year
 	bulan = hari_ini.month
 	hari = hari_ini.day
-	counter = len(contacts) + 1
+	counter = len(daftar_pakaians) + 1
 	first = name[0].upper()
 	last_4 = harga[-4:]
 	id_contact = ("%04d%02d%02d-C%03d%s%s" % (tahun, bulan, hari, counter, first, last_4))
@@ -57,7 +58,9 @@ def add_daftar_pakaian():
 	Harga = input("Harga \t: ")
 	respon = input(f"Apakah anda yakin Menyimpan : {Jenis_pakaian} ? (Y/N)\n")
 	if verify_ans(respon):
-		daftar_pakaians[Jenis_pakaian] = {
+		id_contact = create_id(name=Jenis_pakaian, harga=Harga)
+		daftar_pakaians[id_contact] = {
+			"Nama" : Jenis_pakaian,
 			"Harga" : Harga
 		}
 	else:
@@ -65,8 +68,9 @@ def add_daftar_pakaian():
 	input("Tekan ENTER untuk kembali ke MENU")
 
 def searching(daftar_pakaian):
-	if daftar_pakaian in daftar_pakaians:
-		return True 
+	for id_contact in daftar_pakaians:
+		if daftar_pakaians[id_contact]["Nama"] == daftar_pakaian:
+			return id_contact 
 	else:
 		return False
 
@@ -76,14 +80,42 @@ def find_daftar_pakaian():
 	exists = searching(Jenis_pakaian)
 	if exists:
 		print("Data ditemukan")
-		print_data(daftar_pakaian=Jenis_pakaian)
+		print_data(id_contact=exists)
 	else:
 		print("Data yang dicari tidak ada")
 	input("Tekan ENTER untuk kembali ke MENU")
 
+def update_daftar_pakaian():
+	print(f"Nama Lama : {daftar_pakaians[Jenis_pakaian]}")
+	new_name = input("Masukkan Nama baru : ")
+	respon = input("Apakah yakin data ingin diubah (Y/N) : ")
+	result = verify_ans(respon)
+	if result :
+		daftar_pakaians[Jenis_pakaian] = new_name
+		
+		print("Data Telah di simpan")
+		print_data(id_contact)
+	else:
+		print("Data Batal diubah")
+def delete_pakaian():
+	print_header("MENGHAPUS Daftar ITEM")
+	nama = input("Nama Item yang akan Dihapus : ")
+	exists = searching(daftar_pakaians)
+	if exists:
+		print_data(id_contact=exists)
+		respon = input(f"Yakin menghapus {nama} ? (Y/N)")
+		if verify_ans(respon):
+			del daftar_pakaians[exists]
+			print("Data Telah dihpus")
+		else:
+			print("Data Batal dihapus")
+	else:
+		print("Data tidak ada")
+	input("Tekan ENTER untuk kembali ke MENU")
+
 def check_user_input(char):
 	if char == "Q":
-		print("Bye..")
+		print("Semoga harimu menyenangkan... Bye!!!.....")
 		return True
 	elif char == "1":
 		view_daftar_pakaian()
@@ -92,9 +124,9 @@ def check_user_input(char):
 	elif char == "3":
 		find_daftar_pakaian()
 	elif char == "4":
-		()
+		update_daftar_pakaian()
 	elif char == "5":
-		()
+		delete_pakaian()
 
 daftar_pakaians ={
 	"Tshirt" : {
